2f07c7a243bcd583d657a8a942fb92d2aa4b232c
judamo <judamo18@gmail.com>
2016-09-14 20:39:24 -0500

    crear clase persona
    se crea una clase Persona.java con los sigtes atributos(id, nombre y clave)
    
    tipo: nueva funcionalidad

---
+++ appjava/src/modelo/Persona.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author juan
+ */
+public class Persona {
+    
+    private String id;
+    private String usuario;
+    private String clave;
+
+    public Persona(String id, String usuario, String clave) {
+        this.id = id;
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+    
+}
